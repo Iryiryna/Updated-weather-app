66577dfd243f99bf3bdf8cc34a3e0e97fd553ed2
Iryiryna <118272871+Iryiryna@users.noreply.github.com>
2023-03-31 22:49:45 +0300

    Added forecast api

--- src/app.js
+++ src/app.js
@@ -21,6 +21,7 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 function displayForecast() {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let days = ["Fri", "Sat", "Sun", "Mon"];
   
@@ -72,6 +73,16 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
+ function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "9fe6a3db05bc74b4bed813ab153733eb";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+ 
+ axios.get(apiUrl).then(displayForecast);
+
+}
+
+
 function search(city) {
   let apiKey = "9fe6a3db05bc74b4bed813ab153733eb";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
@@ -113,4 +124,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Kyiv");
-displayForecast();
